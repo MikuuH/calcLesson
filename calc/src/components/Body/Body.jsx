f7e69c6c30836044a98ba3aeceba3667d012122a
miku <39mkui@gmail.com>
2024-07-07 17:24:53 +0300

    add default

---
+++ calc/src/components/Body/Body.jsx
@@ -0,0 +1,11 @@
+import Calculator from '../Calculator/mainCalc'
+import './Body.css'
+export default function Body() {
+    return (
+        <>
+            <body>
+                <Calculator />
+            </body>
+        </>
+    )
+}
---
+++ calc/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+
+export default function Header() {
+    return (
+      <>
+        <div>
+          <header>
+                <h1>CALCULATOR</h1>
+          </header>
+        </div>
+      </>
+    );
+  }
